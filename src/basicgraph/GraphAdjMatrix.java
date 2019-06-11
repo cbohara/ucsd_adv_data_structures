a073fe1d0742d07988623a90bb4c4317c5aa172c
Charlie O'Hara <charlie.b.ohara@gmail.com>
2019-06-10 20:52:46 -0700

    week 2

--- src/basicgraph/GraphAdjMatrix.java
+++ src/basicgraph/GraphAdjMatrix.java
@@ -111,8 +111,13 @@ public class GraphAdjMatrix extends Graph {
 	 * @return List<Integer> a list of indices of vertices.  
 	 */	
 	public List<Integer> getDistance2(int v) {
-		// XXX Implement this method in week 2
-		return null;
+		List<Integer> twoHop = new ArrayList<Integer>();
+		for (int i = 0; i < getNumVertices(); i++) {
+			for (int j = 0; j < adjMatrix[v][i]; j++) {
+				twoHop.addAll(getNeighbors(i));
+			}
+		}
+		return twoHop;
 	}
 	
 	/**
